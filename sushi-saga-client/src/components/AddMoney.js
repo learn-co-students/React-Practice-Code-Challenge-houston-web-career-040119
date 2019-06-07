06b868802531b1b865f616b582067271a007b62e
kreestoff <christopher.l.mcintosh@outlook.com>
2019-06-07 17:57:31 -0500

    done

---
+++ sushi-saga-client/src/components/AddMoney.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddMoney = (props) => {
+    return <button onClick={props.addMoney}>
+            More Money!
+          </button>
+}
+
+export default AddMoney
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
+import AddMoney from '../components/AddMoney'
 
 const SushiContainer = (props) => {
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushi.map(sushi => <Sushi sushi={sushi} eatSushi={props.eatSushi}/>)}
+        <MoreButton getMore={props.getMore} />
+        <AddMoney addMoney={props.addMoney} />
       </div>
     </Fragment>
   )
