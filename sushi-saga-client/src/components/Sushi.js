3849e02a80c589d9e7e84cc67fe68c96c940dd0e
github username <github email address>
2019-10-12 22:33:40 -0500

    Done.

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,27 @@
 import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
+  
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={props.eatSushi}>
+       
+             
         { 
           /* Tell me if this sushi has been eaten! */ 
           true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
+          <img src={props.sushi.img_url} width="100%" />
+          : null
+            // <img src={/* Give me an image source! */} width="100%" />
         }
       </div>
+
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+      {props.sushi.name} - ${props.sushi.price}
       </h4>
     </div>
   )
 }
 
-export default Sushi
+export default Sushi;
